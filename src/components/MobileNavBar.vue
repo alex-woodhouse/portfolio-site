b30e9abefc06f7ca131aa7fa5de7aac0a8d850fd
Alex Woodhouse <contact@alexwoodhouse.co.uk>
2021-10-17 20:25:06 +0100

    Add nav bar and mobile nav bar

---
+++ src/components/MobileNavBar.vue
@@ -0,0 +1,14 @@
+<template>
+
+
+</template>
+
+<script>
+export default {
+  name: "MobileNavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div >
+    <ul>
+      <li class="active"><a href="">Home</a></li>
+      <li><a href="#about">About Me</a></li>
+      <li><a href="">My Works</a></li>
+      <li><a href="">Contact Me</a></li>
+      <li><a href="">Blog</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 4px 16px;
+  text-decoration: none;
+}
+
+.active {
+  border-radius: 10px;
+  border: 2px solid #FF1E56;
+}
+</style>
